8fcfa8b0e84a383a668088a92a2f0e0589cf1361
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-01-19 19:25:34 +0300

    Card component

---
+++ src/tailwind-mobile/components/Card.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { classNames } from '../shared/classnames';
+
+const Card = (props) => {
+  const {
+    tag = 'div',
+    className,
+    colors: colorsProp,
+    header,
+    footer,
+
+    // Theme
+    ios,
+    material,
+
+    // Style
+    outline,
+
+    // Children
+    children,
+
+    // Rest
+    ...rest
+  } = props;
+
+  const Component = tag;
+
+  const attrs = {
+    ...rest,
+  };
+
+  const theme = ios ? 'ios' : material ? 'material' : 'common';
+
+  const colors = {
+    bg: 'bg-white',
+    border: 'border-gray-200',
+    footerText: 'text-black',
+    ...colorsProp,
+  };
+
+  const style = outline ? 'outline' : 'shadow';
+
+  const c = {
+    base: {
+      initial: `m-4 ${colors.bg} rounded overflow-hidden`,
+      ios: ``,
+      material: ``,
+      common: ``,
+    },
+    style: {
+      shadow: 'shadow',
+      outline: `border ${colors.border}`,
+    },
+    header: 'border-b px-4 py-2',
+    content: 'p-4 text-sm',
+    footer: `border-t ${colors.footerText} text-sm text-opacity-55 px-4 py-2`,
+  };
+
+  const classes = classNames([
+    // base
+    c.base.initial,
+    c.base[theme],
+
+    c.style[style],
+    className,
+  ]);
+
+  const headerClasses = classNames([c.header]);
+  const contentClasses = classNames([c.content]);
+  const footerClasses = classNames([c.footer]);
+
+  return (
+    <Component className={classes} {...attrs}>
+      {header && <div className={headerClasses}>{header}</div>}
+      <div className={contentClasses}>{children}</div>
+      {footer && <div className={footerClasses}>{footer}</div>}
+    </Component>
+  );
+};
+
+export default Card;
