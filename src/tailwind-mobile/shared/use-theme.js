db867859ded44ae9731e10fbc9dc07d283ae9651
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-01-26 20:52:16 +0300

    Move themeClasses to useTheme

--- src/tailwind-mobile/shared/use-theme.js
+++ src/tailwind-mobile/shared/use-theme.js
@@ -1,16 +1,27 @@
 import { useContext } from 'react';
-import { themeCls } from './theme-cls';
 import { ThemeContext } from './theme-context';
+import { cls } from './cls';
+
+const themeClasses = (classesObj, theme, state) => {
+  if (typeof classesObj === 'string') return classesObj;
+  const args = [classesObj.initial, classesObj[theme]];
+  if (state && classesObj[state]) {
+    if (typeof classesObj[state] === 'string') args.push(classesObj[state]);
+    else {
+      args.push(classesObj[state].initial, classesObj[state][theme]);
+    }
+  }
+  return cls(...args);
+};
 
 const useTheme = ({ ios, material }) => {
   const themeContext = useContext(ThemeContext);
-  let theme;
+  let theme = themeContext || 'common';
   if (ios) theme = 'ios';
   if (material) theme = 'material';
-  theme = themeContext || 'common';
   return {
     theme,
-    themeClasses: themeCls(theme),
+    themeClasses: (classesObj, state) => themeClasses(classesObj, theme, state),
   };
 };
 
