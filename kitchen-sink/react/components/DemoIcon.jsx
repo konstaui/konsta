eb23d7c83cea203572892b7c0e5ec1b331c82da9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-02-20 00:45:54 +0300

    new demo icon

--- kitchen-sink/react/components/DemoIcon.jsx
+++ kitchen-sink/react/components/DemoIcon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import IconSrc from '../images/f7-icon.png';
+import IconSrc from '../images/demo-icon.png';
 
 const DemoIcon = () => {
   return <img src={IconSrc} alt="icon" className="ios:w-7 material:w-6" />;
