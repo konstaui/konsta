a8625d35f89e8866e24809b66abff2f55b43861d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-02-19 15:20:41 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,11 @@ const tailwindMobile = require('./src/config');
 module.exports = tailwindMobile({
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {
+      width: ['ios', 'material'],
+      height: ['ios', 'material'],
+      inset: ['ios', 'material'],
+    },
+  },
 });
