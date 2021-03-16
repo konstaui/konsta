9a3c0ff4866a23547b57c782df9ac81dcfe71ba4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-03-16 17:51:21 +0300

    Update extend-theme.js

--- src/config/extend-theme.js
+++ src/config/extend-theme.js
@@ -1,6 +1,6 @@
 const extendTheme = () => {
   // SIZES
-  const sizesValues = [0.25, 0.75, 4.5, 5.5, 5.75, 7.5, 12.5, 13, 15, 18];
+  const sizesValues = [0.25, 0.75, 4.5, 5.5, 5.75, 7.5, 12.5, 13, 15, 18, 160];
   const minSizesValues = Array.from({ length: 12 }).map(
     (el, index) => index + 1
   );
@@ -8,12 +8,13 @@ const extendTheme = () => {
   const spacing = {
     '1em': '1em',
   };
+  const minWidth = {};
+  const minHeight = {};
+
   sizesValues.forEach((size) => {
     spacing[size] = `${size / 4}rem`;
   });
 
-  const minWidth = {};
-  const minHeight = {};
   minSizesValues.forEach((size) => {
     minWidth[size] = `${size / 4}rem`;
     minHeight[size] = `${size / 4}rem`;
@@ -37,26 +38,30 @@ const extendTheme = () => {
   });
 
   // TRANSITION
-  const durationValues = [0];
+  const durationValues = [0, 400];
   const transitionDuration = {};
   durationValues.forEach((value) => {
     transitionDuration[value] = `${value}ms`;
   });
 
+  // FONT SIZE
   const fontSize = {
     '2xs': '0.625rem',
     'icon-ios': '28px',
     'icon-material': '24px',
   };
 
+  // SCALE
   const scale = {
     133: `${16 / 12}`,
   };
 
+  // BOX SHADOW
   const boxShadow = {
     'ios-toggle': '0 2px 4px rgba(0,0,0,.3)',
   };
 
+  // COLORS
   const colors = {
     'page-ios-light': '#efeff4',
     'page-ios-dark': '#000',
