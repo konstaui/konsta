9e8fc9c1118f8d781314af619cf2590c63494f1c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-08-26 19:20:15 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const tailwindMobile = require('tailwind-mobile/config');
 
 module.exports = tailwindMobile({
   mode: 'jit',
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './kitchen-sink/react/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-  ],
+  purge: ['./kitchen-sink/react/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   variants: {
     extend: {
