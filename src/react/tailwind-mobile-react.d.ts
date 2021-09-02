aa19ae79cf5e64ed35a948b6a9a818385402d461
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-09-02 15:55:31 +0300

    TailwindMobileProviderProps docs

--- src/react/tailwind-mobile-react.d.ts
+++ src/react/tailwind-mobile-react.d.ts
@@ -3,8 +3,24 @@ import * as React from 'react';
 
 // PROVIDER
 interface TailwindMobileProviderProps {
+  /**
+   * App theme
+   *
+   * @default 'material'
+   */
   theme?: 'ios' | 'material';
+  /**
+   * Include `dark:` variants (if dark theme is in use)
+   *
+   * @default false
+   * */
   dark?: boolean;
+  /**
+   * Enables touch ripple effect in Material theme. Allows to globally disable touch ripple for all components
+   *
+   * @default true
+   */
+  touchRipple?: boolean;
 }
 declare const TailwindMobileProvider: React.FunctionComponent<TailwindMobileProviderProps>;
 
