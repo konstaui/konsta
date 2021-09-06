7989b4ab8e95181f2d491b48d26b738ef66ebc16
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-09-06 14:56:36 +0300

    fix(provider): fix touchRipple defaults

--- src/react/shared/TailwindMobileProvider.jsx
+++ src/react/shared/TailwindMobileProvider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TailwindMobileContext } from './TailwindMobileContext.js';
 
 const TailwindMobileProvider = (props) => {
-  const { theme, dark, touchRipple, children } = props;
+  const { theme, dark, touchRipple = true, children } = props;
 
   return (
     <TailwindMobileContext.Provider value={{ theme, dark, touchRipple }}>
