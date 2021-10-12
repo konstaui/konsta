ca466a89eb4b3ad5b0bcf0e730ff8aa7e9c15cd0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-10-12 13:23:39 +0300

    chore: vue package and vue types

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,16 @@
 const buildCopy = require('./build-copy.js');
 const buildReact = require('./build-react.js');
+const buildVue = require('./build-vue.js');
 
 const formats = ['esm', 'cjs'];
+
 (async () => {
   const env = process.env.NODE_ENV || 'development';
   const outputDir = env === 'development' ? 'build' : 'package';
   return Promise.all([
     buildCopy(),
     ...formats.map((format) => buildReact(format, outputDir)),
+    ...formats.map((format) => buildVue(format, outputDir)),
   ]).catch((err) => {
     // eslint-disable-next-line
     console.error(err);
---
+++ src/vue/tailwind-mobile-vue.d.ts
@@ -0,0 +1,48 @@
+import { Ref, ComponentOptionsMixin, DefineComponent } from 'vue';
+// IMPORT_COMPONENTS
+
+// PROVIDER
+declare const TailwindMobileProvider: DefineComponent<
+  {
+    /**
+     * App theme. If set to `'parent'` it will look for `ios` or `md` class on root `<html>` element, useful to use with parent framework like Framework7 or Ionic
+     *
+     * @default 'material'
+     */
+    theme: {
+      type: StringConstructor;
+      default: 'material';
+    };
+    /**
+     * Include `dark:` variants (if dark theme is in use)
+     *
+     * @default false
+     * */
+    dark: {
+      type: BooleanConstructor;
+      default: false;
+    };
+    /**
+     * Enables touch ripple effect in Material theme. Allows to globally disable touch ripple for all components
+     *
+     * @default true
+     */
+    touchRipple: {
+      type: BooleanConstructor;
+      default: true;
+    };
+  },
+  () => JSX.Element,
+  unknown,
+  {},
+  {},
+  ComponentOptionsMixin,
+  ComponentOptionsMixin,
+  {}
+>;
+
+// HELPERS
+declare const useTheme: () => Ref<'material' | 'ios'>;
+
+// EXPORT_COMPONENTS
+export { useTheme, TailwindMobileProvider };
