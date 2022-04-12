8135ac7c44df2e6565e7b4bc52953ecec4aaa35d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-04-12 12:34:57 +0300

    fix(preloader): fix iOS preloader animation in Safari

--- src/config/plugin-preloader.js
+++ src/config/plugin-preloader.js
@@ -22,6 +22,9 @@ module.exports = () =>
       },
 
       '@keyframes k-ios-preloader-spin': {
+        '0%': {
+          transform: 'rotate(0deg)',
+        },
         '100%': {
           transform: 'rotate(360deg)',
         },
