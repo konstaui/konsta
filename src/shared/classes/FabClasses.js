f5f1fb481786a298ac2ac81a390729d42ae82510
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-08-08 17:16:52 +0300

    feat(fab): m3 appearance

--- src/shared/classes/FabClasses.js
+++ src/shared/classes/FabClasses.js
@@ -1,22 +1,22 @@
 export const FabClasses = (props, colors) => {
   return {
     base: {
-      common: `${colors.bg} ${colors.activeBg} ${colors.text} flex items-center justify-center space-x-2 rounded-full shadow-lg cursor-pointer overflow-hidden select-none`,
-      ios: `h-12 duration-100`,
-      material: `duration-300 touch-ripple-white`,
+      common: `${colors.bg} ${colors.activeBg} ${colors.text} flex items-center justify-center space-x-2 cursor-pointer overflow-hidden select-none`,
+      ios: `h-12 duration-100 rounded-full shadow-lg`,
+      material: `duration-300 touch-ripple-white rounded-2xl shadow`,
       iconOnly: {
         ios: 'w-12',
         material: 'w-14 h-14',
       },
       withText: {
         common: 'px-4',
-        material: 'h-12',
+        material: 'h-14',
       },
     },
     text: {
-      common: 'uppercase text-sm',
-      ios: 'font-semibold',
-      material: 'font-medium tracking-wider',
+      common: 'text-sm',
+      ios: 'font-semibold uppercase',
+      material: 'font-medium',
     },
     icon: {
       common: 'h-1em w-1em',
