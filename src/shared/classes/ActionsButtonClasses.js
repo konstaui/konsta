96c438554ce5fff0b0ba039fc9fd2768bfd53657
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-08-11 15:44:04 +0300

    feat(actions): m3 styles

--- src/shared/classes/ActionsButtonClasses.js
+++ src/shared/classes/ActionsButtonClasses.js
@@ -5,19 +5,22 @@ export const ActionsButtonClasses = (props, colors, darkClasses) => {
   return {
     base: {
       common: cls(
-        colors.bg,
         colors.text,
-        `flex items-center w-full px-4 relative z-10 overflow-hidden ${colors.activeBg}`,
+        `flex items-center w-full px-4 relative z-10 overflow-hidden`,
         bold && 'font-semibold'
       ),
       ios: cls(
         `h-14`,
+        colors.bgIos,
+        colors.activeBgIos,
         fontSizeIos,
         hairlines && 'hairline-b',
         'first:rounded-t-xl last:rounded-b-xl justify-center'
       ),
       material: cls(
         `h-12`,
+        colors.bgMaterial,
+        colors.activeBgMaterial,
         fontSizeMaterial,
         'justify-start',
         darkClasses('dark:touch-ripple-white')
--- src/shared/colors/ActionsButtonColors.js
+++ src/shared/colors/ActionsButtonColors.js
@@ -2,8 +2,13 @@ import { cls } from '../cls.js';
 
 export const ActionsButtonColors = (colorsProp = {}, dark) => {
   return {
-    bg: cls('bg-white', dark('dark:bg-neutral-800')),
-    activeBg: cls('active:bg-neutral-200', dark('dark:active:bg-neutral-700')),
+    bgIos: cls('bg-white', dark('dark:bg-neutral-800')),
+    bgMaterial: cls('bg-white', dark('dark:bg-neutral-800')),
+    activeBgIos: cls(
+      'active:bg-neutral-200',
+      dark('dark:active:bg-neutral-700')
+    ),
+    activeBgMaterial: '',
     text: 'text-primary',
     ...colorsProp,
   };
