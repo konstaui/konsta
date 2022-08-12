cdd6cdb51cab3e8a073177799f55386cde2bed9c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-08-12 19:34:29 +0300

    feat(toolbar): m3 color

--- src/shared/colors/ToolbarColors.js
+++ src/shared/colors/ToolbarColors.js
@@ -3,10 +3,7 @@ import { cls } from '../cls.js';
 export const ToolbarColors = (colorsProp = {}, dark) => {
   return {
     bgIos: cls('bg-bars-ios-light', dark('dark:bg-bars-ios-dark')),
-    bgMaterial: cls(
-      'bg-bars-material-light',
-      dark('dark:bg-bars-material-dark')
-    ),
+    bgMaterial: cls('bg-md-light-surface-2', dark('dark:bg-md-dark-surface-2')),
     ...colorsProp,
   };
 };
