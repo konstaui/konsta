8e3d5e98ae38650a7707dc45b5bce7a82a1de4cc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-08-12 14:21:58 +0300

    chore: tweak sheet colors

--- src/shared/colors/SheetColors.js
+++ src/shared/colors/SheetColors.js
@@ -3,7 +3,7 @@ import { cls } from '../cls.js';
 export const SheetColors = (colorsProp = {}, dark) => {
   return {
     bgIos: cls('bg-white', dark('dark:bg-black')),
-    bgMaterial: cls('bg-md-light-surface-2', dark('dark:bg-md-dark-surface-2')),
+    bgMaterial: cls('bg-md-light-surface', dark('dark:bg-md-dark-surface')),
     ...colorsProp,
   };
 };
