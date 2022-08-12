ac02418760f3d876650bd382df3b66bf6d6ef7ae
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-08-12 19:37:28 +0300

    feat(list-button): tweak list button font weight

--- src/shared/classes/ListButtonClasses.js
+++ src/shared/classes/ListButtonClasses.js
@@ -12,11 +12,7 @@ export const ListButtonClasses = (props, colors, classes) => {
         `flex items-center justify-center px-4 space-x-1 w-full duration-300 active:duration-0 focus:outline-none ${colors.touchRipple} overflow-hidden select-none`
       ),
       ios: cls('h-11', colors.textIos, colors.activeBgIos, colors.activeBgIos),
-      material: cls(
-        'h-12 font-medium',
-        colors.textMaterial,
-        colors.activeBgMaterial
-      ),
+      material: cls('h-12', colors.textMaterial, colors.activeBgMaterial),
     },
   };
 };
