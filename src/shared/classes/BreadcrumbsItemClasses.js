5c30bb7b892caafcec700103f1c17f1d4a0bc1fd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-04-14 14:17:39 +0300

    feat(breadcrumbs): breadcrumbs-item now has overflow-hidden

--- src/shared/classes/BreadcrumbsItemClasses.js
+++ src/shared/classes/BreadcrumbsItemClasses.js
@@ -4,7 +4,7 @@ export const BreadcrumbsItemClasses = (props, colors) => {
   const { active } = props;
   return {
     base: {
-      common: cls(`flex items-center`),
+      common: cls(`flex items-center overflow-hidden`),
       ios: cls(
         colors.textIos,
         colors.bgIos,
