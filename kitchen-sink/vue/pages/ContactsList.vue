943f78f5ab03fa701dfe184af3945714e22a0d19
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-07-31 12:11:49 +0300

    fix vue listgroup to receive same list props

--- kitchen-sink/vue/pages/ContactsList.vue
+++ kitchen-sink/vue/pages/ContactsList.vue
@@ -6,39 +6,39 @@
       </template>
     </k-navbar>
     <k-list strong-ios>
-      <k-list-group>
-        <k-list-item title="A" contacts group-title :dividers="false" />
-        <k-list-item title="Aaron" contacts :dividers="false" />
-        <k-list-item title="Abbie" contacts :dividers="false" />
-        <k-list-item title="Adam" contacts :dividers="false" />
-        <k-list-item title="Adele" contacts :dividers="false" />
-        <k-list-item title="Agatha" contacts :dividers="false" />
-        <k-list-item title="Agnes" contacts :dividers="false" />
-        <k-list-item title="Albert" contacts :dividers="false" />
-        <k-list-item title="Alexander" contacts :dividers="false" />
+      <k-list-group :dividers="false">
+        <k-list-item title="A" contacts group-title />
+        <k-list-item title="Aaron" contacts />
+        <k-list-item title="Abbie" contacts />
+        <k-list-item title="Adam" contacts />
+        <k-list-item title="Adele" contacts />
+        <k-list-item title="Agatha" contacts />
+        <k-list-item title="Agnes" contacts />
+        <k-list-item title="Albert" contacts />
+        <k-list-item title="Alexander" contacts />
       </k-list-group>
-      <k-list-group>
-        <k-list-item title="B" group-title contacts :dividers="false" />
-        <k-list-item title="Bailey" contacts :dividers="false" />
-        <k-list-item title="Barclay" contacts :dividers="false" />
-        <k-list-item title="Bartolo" contacts :dividers="false" />
-        <k-list-item title="Bellamy" contacts :dividers="false" />
-        <k-list-item title="Belle" contacts :dividers="false" />
-        <k-list-item title="Benjamin" contacts :dividers="false" />
+      <k-list-group :dividers="false">
+        <k-list-item title="B" group-title contacts />
+        <k-list-item title="Bailey" contacts />
+        <k-list-item title="Barclay" contacts />
+        <k-list-item title="Bartolo" contacts />
+        <k-list-item title="Bellamy" contacts />
+        <k-list-item title="Belle" contacts />
+        <k-list-item title="Benjamin" contacts />
       </k-list-group>
-      <k-list-group>
-        <k-list-item title="C" group-title contacts :dividers="false" />
-        <k-list-item title="Caiden" contacts :dividers="false" />
-        <k-list-item title="Calvin" contacts :dividers="false" />
-        <k-list-item title="Candy" contacts :dividers="false" />
-        <k-list-item title="Carl" contacts :dividers="false" />
-        <k-list-item title="Cherilyn" contacts :dividers="false" />
-        <k-list-item title="Chester" contacts :dividers="false" />
-        <k-list-item title="Chloe" contacts :dividers="false" />
+      <k-list-group :dividers="false">
+        <k-list-item title="C" group-title contacts />
+        <k-list-item title="Caiden" contacts />
+        <k-list-item title="Calvin" contacts />
+        <k-list-item title="Candy" contacts />
+        <k-list-item title="Carl" contacts />
+        <k-list-item title="Cherilyn" contacts />
+        <k-list-item title="Chester" contacts />
+        <k-list-item title="Chloe" contacts />
       </k-list-group>
-      <k-list-group>
-        <k-list-item title="V" group-title contacts :dividers="false" />
-        <k-list-item title="Vladimir" contacts :dividers="false" />
+      <k-list-group :dividers="false">
+        <k-list-item title="V" group-title contacts />
+        <k-list-item title="Vladimir" contacts />
       </k-list-group>
     </k-list>
   </k-page>
--- src/vue/components/ListGroup.vue
+++ src/vue/components/ListGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <k-list nested>
+    <k-list nested v-bind="props">
       <slot />
     </k-list>
   </li>
@@ -12,6 +12,27 @@
     name: 'k-list-group',
     components: {
       kList,
+    },
+    props: {
+      margin: { type: String, default: 'my-8' },
+      dividers: { type: Boolean, default: undefined },
+      dividersIos: { type: Boolean, default: true },
+      dividersMaterial: { type: Boolean, default: false },
+      inset: { type: Boolean, default: undefined },
+      insetIos: { type: Boolean, default: false },
+      insetMaterial: { type: Boolean, default: false },
+      strong: { type: Boolean, default: undefined },
+      strongIos: { type: Boolean, default: false },
+      strongMaterial: { type: Boolean, default: false },
+      outline: { type: Boolean, default: undefined },
+      outlineIos: { type: Boolean, default: false },
+      outlineMaterial: { type: Boolean, default: false },
+      menuList: { type: Boolean, default: false },
+    },
+    setup(props) {
+      return {
+        props,
+      };
     },
   };
 </script>
