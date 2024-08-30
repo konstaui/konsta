3f873e9200b2d2c9195349c244b7405fcaa5f56c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2024-08-30 13:20:14 +0300

    chore: update to Svelte 5

--- kitchen-sink/svelte/index.js
+++ kitchen-sink/svelte/index.js
@@ -1,7 +1,8 @@
-import './styles/index.css';
+import { mount } from 'svelte';
 import App from './components/App.svelte';
+import './styles/index.css';
 
 // eslint-disable-next-line
-const app = new App({
+const app = mount(App, {
   target: document.querySelector('#app'),
 });
