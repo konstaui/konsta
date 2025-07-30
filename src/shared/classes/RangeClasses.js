f66187324217c49c4263cf9500c85521403bdf53
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-07-30 17:46:58 -0400

    feat: update iOS Range styles

--- src/shared/classes/RangeClasses.js
+++ src/shared/classes/RangeClasses.js
@@ -12,12 +12,12 @@ export const RangeClasses = (props, colors, darkClasses) => {
         'absolute top-1/2 left-0 w-full transform -translate-y-1/2 bg-black/20',
         darkClasses('dark:bg-white/20')
       ),
-      ios: 'h-1 rounded-full',
+      ios: 'h-1.5 rounded-full',
       material: 'h-0.5',
     },
     trackValue: {
       common: `absolute top-1/2 start-0 w-full transform -translate-y-1/2`,
-      ios: `h-1 rounded-full ${colors.valueBgIos}`,
+      ios: `h-1.5 rounded-full ${colors.valueBgIos}`,
       material: `h-0.5 ${colors.valueBgMaterial}`,
     },
     input: {
@@ -30,7 +30,7 @@ export const RangeClasses = (props, colors, darkClasses) => {
     inputThumb: {
       common:
         'range-thumb:relative range-thumb:appearance-none range-thumb:rounded-full range-thumb:border-none',
-      ios: `range-thumb:w-7 range-thumb:h-7 range-thumb:-mt-3.5 ${colors.thumbBgIos} range-thumb:shadow-ios-toggle`,
+      ios: `range-thumb:w-9.5 range-thumb:h-6 range-thumb:-mt-3 ${colors.thumbBgIos} range-thumb:shadow-ios-toggle`,
       material: `range-thumb:w-3 range-thumb:h-3 ${colors.thumbBgMaterial} range-thumb:-mt-1.5 range-thumb:transform range-thumb:duration-200 active:range-thumb:scale-150`,
     },
     inputTrack: {
