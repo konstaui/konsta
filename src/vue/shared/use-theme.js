1a708f99c9fb0fea4e5b51f1b3bbd3f8507e37e9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-08-05 11:52:39 -0400

    fix: Vue - fix useTheme util

--- src/vue/shared/use-theme.js
+++ src/vue/shared/use-theme.js
@@ -1,10 +1,18 @@
 import { computed } from 'vue';
+import { useContext } from './use-context.js';
 
 const useTheme = (props = {}, context) =>
   computed(() => {
     let theme;
     if (context) {
       theme = context.value.theme || 'ios';
+    } else {
+      context = useContext();
+      if (context && context.value) {
+        theme = context.value.theme || 'ios';
+      } else {
+        theme = 'ios';
+      }
     }
     if (props.ios) theme = 'ios';
     if (props.material) theme = 'material';
