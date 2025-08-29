a71ba6b07a48527a03401a5d64d3d9ee97c25bb0
David Regla <dreglad@gmail.com>
2025-08-29 08:19:13 -0600

    Update background opacity for Tailwind 4

--- src/shared/classes/SheetClasses.js
+++ src/shared/classes/SheetClasses.js
@@ -14,7 +14,7 @@ export const SheetClasses = (props, colors, baseClassName) => {
     },
     backdrop: {
       common:
-        'fixed z-40 w-full h-full left-0 top-0 bg-black bg-opacity-50 duration-400',
+        'fixed z-40 w-full h-full left-0 top-0 bg-black/50 duration-400',
       opened: '',
       closed: 'opacity-0 pointer-events-none',
     },
