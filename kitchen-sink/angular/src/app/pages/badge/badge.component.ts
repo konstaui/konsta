392af7275949c3e6326b23b27ee035732ba64683
Martin Donadieu <martindonadieu@gmail.com>
2025-10-12 20:56:37 +0100

    fix: badge page

--- kitchen-sink/angular/src/app/pages/badge/badge.component.ts
+++ kitchen-sink/angular/src/app/pages/badge/badge.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, viewChild, TemplateRef } from '@angular/core';
+import { useThemeSignal } from '../../../../../../src/angular/shared/theme-helpers.js';
 import { KPageComponent } from '../../../../../../src/angular/components/page.component.js';
 import { KNavbarComponent } from '../../../../../../src/angular/components/navbar.component.js';
 import { KNavbarBackLinkComponent } from '../../../../../../src/angular/components/navbar-back-link.component.js';
@@ -51,27 +52,55 @@ const DEMO_ICON_SRC = '/demo-icon.png';
       </k-navbar>
 
       <k-tabbar [labels]="true" [icons]="true" class="left-0 bottom-0 fixed">
-        <k-toolbar-pane>
-          <k-tabbar-link [active]="true" [label]="'Inbox'">
-            <span icon class="relative inline-flex">
-              <app-demo-icon />
-              <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-green-500' }">5</k-badge>
-            </span>
-          </k-tabbar-link>
-          <k-tabbar-link [label]="'Calendar'">
-            <span icon class="relative inline-flex">
-              <app-demo-icon />
-              <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-red-500' }">7</k-badge>
-            </span>
-          </k-tabbar-link>
-          <k-tabbar-link [label]="'Upload'">
-            <span icon class="relative inline-flex">
-              <app-demo-icon />
-              <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-red-500' }">1</k-badge>
-            </span>
-          </k-tabbar-link>
+        <k-toolbar-pane [tabbar]="true">
+          <k-tabbar-link
+            [active]="true"
+            [label]="'Inbox'"
+            [icon]="inboxIconTpl"
+          ></k-tabbar-link>
+          <k-tabbar-link
+            [label]="'Calendar'"
+            [icon]="calendarIconTpl"
+          ></k-tabbar-link>
+          <k-tabbar-link
+            [label]="'Upload'"
+            [icon]="uploadIconTpl"
+          ></k-tabbar-link>
         </k-toolbar-pane>
       </k-tabbar>
+
+      <ng-template #inboxIconTpl>
+        <span class="relative inline-flex">
+          @if (isIos()) {
+            <i class="f7-icons w-7 h-7">envelope_fill</i>
+          } @else {
+            <i class="f7-icons w-6 h-6">envelope_fill</i>
+          }
+          <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-green-500' }">5</k-badge>
+        </span>
+      </ng-template>
+
+      <ng-template #calendarIconTpl>
+        <span class="relative inline-flex">
+          @if (isIos()) {
+            <i class="f7-icons w-7 h-7">calendar</i>
+          } @else {
+            <i class="f7-icons w-6 h-6">calendar</i>
+          }
+          <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-red-500' }">7</k-badge>
+        </span>
+      </ng-template>
+
+      <ng-template #uploadIconTpl>
+        <span class="relative inline-flex">
+          @if (isIos()) {
+            <i class="f7-icons w-7 h-7">cloud_upload_fill</i>
+          } @else {
+            <i class="f7-icons w-6 h-6">cloud_upload_fill</i>
+          }
+          <k-badge class="absolute -top-1 -right-1" [colors]="{ bg: 'bg-red-500' }">1</k-badge>
+        </span>
+      </ng-template>
 
       <ng-template #demoIconTemplate>
         <img [src]="demoIconSrc" alt="Demo icon" class="ios:w-7 material:w-6" />
@@ -104,7 +133,12 @@ const DEMO_ICON_SRC = '/demo-icon.png';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BadgeComponent {
+  private readonly theme = useThemeSignal();
+
   readonly demoIconSrc = DEMO_ICON_SRC;
+  readonly inboxIconTpl = viewChild<TemplateRef<any>>('inboxIconTpl');
+  readonly calendarIconTpl = viewChild<TemplateRef<any>>('calendarIconTpl');
+  readonly uploadIconTpl = viewChild<TemplateRef<any>>('uploadIconTpl');
   readonly demoIconTemplate = viewChild<TemplateRef<any>>('demoIconTemplate');
   readonly badge0 = viewChild<TemplateRef<any>>('badge0');
   readonly badgeCEO = viewChild<TemplateRef<any>>('badgeCEO');
@@ -116,6 +150,8 @@ export class BadgeComponent {
       typeof document !== 'undefined' &&
       document.location.href.includes('examplePreview')
   );
+
+  readonly isIos = computed(() => this.theme() === 'ios');
 
   back() {
     if (typeof window !== 'undefined') {
