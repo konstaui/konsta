467a0a3542f81d03adc7d40a440382cc0ad7a897
Mayank Singhal <mayanksalesforce@gmail.com>
2025-11-13 04:10:40 +0000

    svelte kitchensink as pwa for real testing

--- kitchen-sink/svelte/vite.config.js
+++ kitchen-sink/svelte/vite.config.js
@@ -1,4 +1,29 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
-export default { base: '', plugins: [svelte(), tailwindcss()] };
+export default {
+  base: '',
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    VitePWA({
+      manifest: {
+        name: 'Konsta UI Svelte PWA',
+        short_name: 'Konsta UI SveltePWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ff3e00',
+        icons: [
+          {
+            src: 'images/demo-icon.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
+};
