8eb6c28c70b07f3c3c54a22b080ceaa7ecfc622f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2026-04-29 14:03:15 -0400

    feat(package): correct package types for CSS exports

--- scripts/build-copy.js
+++ scripts/build-copy.js
@@ -10,9 +10,22 @@ async function buildCopy(cb) {
 
   fs.copyFileSync(`./src/plugin-colors.js`, `./${outputDir}/plugin-colors.js`);
   fs.copyFileSync(`./src/theme.css`, `./${outputDir}/theme.css`);
+  fs.copyFileSync(`./src/theme.css.d.ts`, `./${outputDir}/theme.css.d.ts`);
   fs.copyFileSync(`./src/react/theme.css`, `./${outputDir}/react/theme.css`);
+  fs.copyFileSync(
+    `./src/react/theme.css.d.ts`,
+    `./${outputDir}/react/theme.css.d.ts`
+  );
   fs.copyFileSync(`./src/vue/theme.css`, `./${outputDir}/vue/theme.css`);
+  fs.copyFileSync(
+    `./src/vue/theme.css.d.ts`,
+    `./${outputDir}/vue/theme.css.d.ts`
+  );
   fs.copyFileSync(`./src/svelte/theme.css`, `./${outputDir}/svelte/theme.css`);
+  fs.copyFileSync(
+    `./src/svelte/theme.css.d.ts`,
+    `./${outputDir}/svelte/theme.css.d.ts`
+  );
   if (cb) cb();
 }
 
